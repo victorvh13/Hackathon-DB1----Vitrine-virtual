886504ddc4c1366d54a7e00174bc0695b3fdfc66
victorvh13 <victorhigovh@gmail.com>
2024-06-01 15:01:22 -0300

    navbarfinish

--- netxoes/src/App.js
+++ netxoes/src/App.js
@@ -1,39 +1,56 @@
+import { faBagShopping, faBars, faSearch, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-          <div className="App">
-      <div className="nav">
-        <h1 className="logo">
-          NET<span>XOES</span>
-        </h1>
-        <nav>
-          <ul>
-            <li><
-              Link to="/">Home</Link>
-            </li>
-            <li><
-              Link to="products">Produtos</Link>
-            </li>
-            <li><
-              Link to="about">Sobre</Link>
-            </li>
-            <li><
-              Link to="contact">Contatos</Link>
-            </li>
-            <li><
-            Link to="contact">Contatos</Link>
-            </li>
-          </ul>
-        </nav>
-        
+      <div className="App">
+        <div className="nav">
+          <div className="inner-content">
+          <h1 className="logo">
+            NET<span>XOES</span>
+          </h1>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="products">Produtos</Link>
+              </li>
+              <li>
+                <Link to="about">Sobre</Link>
+              </li>
+              <li>
+                <Link to="contact">Contatos</Link>
+              </li>
+              <li>
+                <Link to="/account">Conta</Link>
+              </li>
+            </ul>
+          </nav>
+          <div className="navs-icon-container"> 
+            <div className="search-input-container">
+              <input type="search" placeholder="Buscar"/>
+              <FontAwesomeIcon icon={faSearch} />
+            </div>
+            <button className="shopping-cart">
+              <FontAwesomeIcon icon={faShoppingCart}/>
+              <div className="produtcs-count">15</div>
+            </button>
+            <button className="menu-button">
+              <FontAwesomeIcon icon = {faBars} />
+            </button>
+          </div>
+        </div>
+          </div>
       </div>
-    </div>
     </Router>
   );
 }
 
 export default App;
+
 

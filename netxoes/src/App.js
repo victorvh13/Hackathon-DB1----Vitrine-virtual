df62cde06d9dce145b815247838bbc96be581070
victorvh13 <victorhigovh@gmail.com>
2024-06-01 12:17:41 -0300

    navbar

--- netxoes/src/App.js
+++ netxoes/src/App.js
@@ -1,11 +1,39 @@
-
+import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      
+    <Router>
+          <div className="App">
+      <div className="nav">
+        <h1 className="logo">
+          NET<span>XOES</span>
+        </h1>
+        <nav>
+          <ul>
+            <li><
+              Link to="/">Home</Link>
+            </li>
+            <li><
+              Link to="products">Produtos</Link>
+            </li>
+            <li><
+              Link to="about">Sobre</Link>
+            </li>
+            <li><
+              Link to="contact">Contatos</Link>
+            </li>
+            <li><
+            Link to="contact">Contatos</Link>
+            </li>
+          </ul>
+        </nav>
+        
+      </div>
     </div>
+    </Router>
   );
 }
 
 export default App;
+
